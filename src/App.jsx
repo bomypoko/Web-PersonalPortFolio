724a1c970583736f7d59ee989ebf80fe2d5c521c
Ratchasri Kidking <orc_hero@hotmail.com>
2023-02-16 17:38:47 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className='bg-red-300'>Ratchasri Kidking</h1>
+
+    </div>
+  )
+}
+
+export default App
